9a11e996b674e521b0954a5baa20930d91ddb461
limbongan <kbkmlimbongan@gmail.com>
2023-09-16 20:08:56 +0700

    Initial commit

---
+++ app/src/main/java/pmo/if21e/limbonganready/MainActivity.java
@@ -0,0 +1,14 @@
+package pmo.if21e.limbonganready;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
